1d68541d7ca635ceeef272b3c78ecf8c309b4841
Kawaza <mchockey555@gmail.com>
2024-03-24 14:43:08 -0600

    about us section update

---
+++ src/components/Experience.js
@@ -0,0 +1,13 @@
+
+function Experience() {
+ 
+    return (
+      <>
+        <div className="container px-10 py-10 text-white mx-auto items-center mb-40 max-w-3xl">
+            
+        </div>
+      </>
+    );
+  }
+  
+  export default Experience;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Banner from "../components/Banner";
-import { SparklesIcon } from '@heroicons/react/24/outline'
+import Experience from "../components/Experience";
+import { Html5Original, Css3Original, JavascriptOriginal, WordpressPlain, ReactOriginal, TailwindcssOriginal, DiscordjsOriginal, PhpPlain, GitOriginal   } from 'devicons-react';
+
 
 export default function Home() {
     return (
         <div>
             <Banner />
-            <section className="bg-gray-800 py-20">
+            <section className="bg-gray-800 py-2 pt-20">
                 <h2 className="text-4xl md:text-5xl font-bold mb-8 text-white text-center">
                     About <span className="blue-text leading-snug">Me</span>
                 </h2>
-                <div className="container px-10 py-10 text-white grid grid-cols-1 md:grid-cols-2 mx-auto gap-40 items-center">
+                <div className="container px-10 py-10 text-white grid grid-cols-1 md:grid-cols-2 mx-auto gap-40 items-center mb-40">
                     <div>
                         <h3 className="text-3xl md:text-4xl font-bold mb-4 text-white text-left">
                             OK <span className="blue-text leading-snug">LET'S CREATE</span> SOMETHING GREAT.
@@ -31,8 +33,8 @@ export default function Home() {
                             <h4 className="text-center py-3 text-2xl">Projects Completed</h4>
                         </div>
                         <div className="custom-border border rounded-md p-4">
-                            <h3 className="text-center py-3 text-5xl blue-text">3+</h3>
-                            <h4 className="text-center py-3 text-2xl">Years of Work</h4>
+                            <h3 className="text-center py-3 text-5xl blue-text">5+</h3>
+                            <h4 className="text-center py-3 text-2xl">Freelance Projects</h4>
                         </div>
                         <div className="custom-border border rounded-md p-4">
                             <h3 className="text-center py-3 text-5xl blue-text">5+</h3>
@@ -41,6 +43,70 @@ export default function Home() {
                     </div>
                     </div>
                 </div>
+            </section>
+            <section className="bg-gray-900 pt-20 pb-1">
+                    <h2 className="text-4xl md:text-5xl font-bold mb-8 text-white text-center">
+                        My <span className="blue-text leading-snug">Skills</span>
+                    </h2>
+                    <div className="container px-10 py-1 text-white grid mx-auto gap-40 items-center mb-24">
+                    <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+                        <div className="custom-border border rounded-md p-10 ">
+                            <div className="flex justify-center">
+                                <Html5Original color="" size="75" />
+                                <Css3Original color="" size="75" />
+                            </div>
+                            <h4 className="text-center py-3 text-2xl pb-0">HTML & CSS</h4>
+                        </div>
+                        <div className="custom-border border rounded-md p-10 ">
+                            <div className="flex justify-center">
+                                <JavascriptOriginal color="white" size="75" />
+                            </div>
+                            <h4 className="text-center py-3 text-2xl pb-0">Javascript</h4>
+                        </div>
+                        <div className="custom-border border rounded-md p-10 ">
+                            <div className="flex justify-center">
+                                <WordpressPlain color="white" size="75" />
+                            </div>
+                            <h4 className="text-center py-3 text-2xl pb-0">Wordpress</h4>
+                        </div>
+                        <div className="custom-border border rounded-md p-10 ">
+                            <div className="flex justify-center">
+                                <ReactOriginal color="" size="75" />
+                            </div>
+                            <h4 className="text-center py-3 text-2xl pb-0">React</h4>
+                        </div>
+                        <div className="custom-border border rounded-md p-10 ">
+                            <div className="flex justify-center">
+                                <TailwindcssOriginal color="" size="75" />
+                            </div>
+                            <h4 className="text-center py-3 text-2xl pb-0">Tailwind</h4>
+                        </div>
+                        <div className="custom-border border rounded-md p-10 ">
+                            <div className="flex justify-center">
+                                <DiscordjsOriginal color="" size="75" />
+                            </div>
+                            <h4 className="text-center py-3 text-2xl pb-0">Discord.js</h4>
+                        </div>
+                        <div className="custom-border border rounded-md p-10 ">
+                            <div className="flex justify-center">
+                                <PhpPlain  color="white" size="75" />
+                            </div>
+                            <h4 className="text-center py-3 text-2xl pb-0">PHP</h4>
+                        </div>
+                        <div className="custom-border border rounded-md p-10 ">
+                            <div className="flex justify-center">
+                                <GitOriginal color="" size="75" />
+                            </div>
+                            <h4 className="text-center py-3 text-2xl pb-0">Git</h4>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-gray-800 py-20">
+                    <h2 className="text-4xl md:text-5xl font-bold mb-8 text-white text-center">
+                        My <span className="blue-text leading-snug">Experience</span>
+                    </h2>
+                    <Experience></Experience>
             </section>
         </div>
     )
