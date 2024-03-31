450895370f16a2e6ef162d61cfe07759b0733d40
Kawaza <mchockey555@gmail.com>
2024-03-30 21:53:52 -0600

    project button update

--- src/pages/projects/project.js
+++ src/pages/projects/project.js
@@ -45,10 +45,16 @@ const ProjectList = () => {
                             <h1 className="text-6xl text-white">{project.title}</h1>
                             <h4 className="blue-text py-4 uppercase">{project.builtWith}</h4>
                         </div>
-                        <a href={project.link} class="mt-14 md:mt-0 align-middle text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-8 rounded-full bg-transparent text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none flex items-center gap-3 h-12 border-white border-2 hover-blue-background duration-300 self-center">
+                        <button onClick={(e) => {
+                            e.preventDefault(); 
+                            if (project.link) {
+                                window.location.href = project.link;
+                                console.log(project.link);
+                            }
+                        }} disabled={!project.link} class="mt-14 md:mt-0 align-middle text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-8 rounded-full bg-transparent text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none flex items-center gap-3 h-12 border-white border-2 hover-blue-background duration-300 self-center">
                             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 8-4 4 4 4m8 0 4-4-4-4m-2-3-4 14"/> </svg>
                             View Website / Code
-                    </a>
+                        </button>
                     </div>
                     <div className="container mx-auto pb-0 px-10 md:px-24">
                         <img className="w-full bg-gray-1000" src={project.bannerimage} alt={project.title} />
