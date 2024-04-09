d6ce37a4b344af88e173dcd9adcc0aef02a5f805
Kawaza <mchockey555@gmail.com>
2024-04-08 22:30:24 -0600

    fix7

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,6 +1,9 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
+  project: {
+    basePath: '/studio'
+  },
   api: {
     projectId: '051kmgbh',
     dataset: 'production'
