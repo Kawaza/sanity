a1bd79b5327c6abe0893d2e61128a1d2bfb11219
Kawaza <mchockey555@gmail.com>
2024-08-06 13:46:13 -0600

    new stuff again

--- src/components/bulkLighthouse.js
+++ src/components/bulkLighthouse.js
@@ -17,6 +17,9 @@ export default function Lighthouse() {
 
     return (
         <div className="mx-auto pt-56 bg-gray-900">
+            <h2 className="text-4xl md:text-5xl font-bold mb-8 text-white text-center">
+                Batch Report <span className="blue-text leading-snug">Lighthouse</span>
+            </h2>
             <form onSubmit={handleSubmit} className="text-center">
                 <textarea
                     value={urls}
